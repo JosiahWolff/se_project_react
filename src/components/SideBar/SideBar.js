9493c9467d7a27ff3cf3eb1aa7229d557e355393
Josiah Wolff <josiahjaw0@gmail.com>
2024-03-04 22:38:44 -0600

    update

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,13 +1,27 @@
 import "./SideBar.css";
-import avatar from "../../images/avatar.png";
-import React from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
+import React, { useContext } from "react";
 
-const SideBar = () => {
+const SideBar = ({ editProfile, logout }) => {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <div className="profile__info">
       <div className="profile__info-container">
-        <img className="profile__avatar" src={avatar} alt="profile avatar" />
-        <p className="profile__user-name">Terrence Tegegne</p>
+        <img
+          className="profile__avatar"
+          src={currentUser?.avatar}
+          alt="profile avatar"
+        ></img>
+        <p className="profile__user-name">{currentUser?.name}</p>
+      </div>
+      <div className="profile__options">
+        <button className="profile__button" onClick={editProfile}>
+          Change Profile Data
+        </button>
+        <button className="profile__button" onClick={logout}>
+          Log out
+        </button>
       </div>
     </div>
   );
