8f8cc3c4e2ee6331449edc1bebff1655d2f52bce
Josiah Wolff <josiahjaw0@gmail.com>
2024-05-27 22:20:46 -0500

    update baseurl

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.wtwrByJosiah3311.crabdance.com"
+    ? "https://api.wtwrbyjosiah3311.crabdance.com"
     : "http://localhost:3001";
 
 export function checkResponse(res) {
